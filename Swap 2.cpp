79d68168952ba763865cb4990b485987131ec256
HASHAM HASSAN <hashamhassan987@gmail.com>
2025-11-16 03:47:03 +0500

    Add files via upload

---
+++ Swap 2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void reverseArray(int arr[], int sz){
+
+int start=0; int end= sz-1;
+while(start<end){
+	swap(arr[start],arr[end]);
+	start++;
+	end--;
+}
+}
+int main() {
+	int arr[]={1,2,3,4,5,6,7,8};
+	int sz= 8;
+reverseArray(arr,sz);
+for(int i=0; i<sz; i++){
+	cout<<arr[i]<<""<<endl;
+	
+}
+	
+	return 0;
+	
+	
+	
+}
