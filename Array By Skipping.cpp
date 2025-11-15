e00eb01e1dd05a7789327d745ea09e5d9f5e23f8
HASHAM HASSAN <hashamhassan987@gmail.com>
2025-11-16 03:48:24 +0500

    Add files via upload

---
+++ Array By Skipping.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int arr[]={11,12,13,14,15};
+	int size=6;
+	cout<<"By skipping one value continousaly: "<<endl;
+	for(int i=0; i<size; i+=2){
+	cout<<	arr[i]<<""<<endl;;
+	}	
+return 0;	
+	
+	
+	
+}
